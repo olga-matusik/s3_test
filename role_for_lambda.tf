d1c0ba40e3d3473909d8f89fa92833f19ecb35fa
olga-matusik <olga.matusik@cloudreach.com>
2022-11-02 12:25:02 +0100

    adding role for lambda

---
+++ role_for_lambda.tf
@@ -0,0 +1,46 @@
+data "aws_iam_policy_document" "gr4_lambda_assume_role_policies" {
+    statement {
+        actions =["sts:AssumeRole"]
+        principals {
+          type = "Service"
+          identifiers = ["lambda.amazonaws.com"]
+        }
+    }
+}
+
+data "aws_iam_policy_document" "gr4_lambda_policies" {
+    statement {
+        sid = "AllObjectActions"
+        effect = "Allow"
+        actions = ["s3:GetObject"]
+        resources = ["arn:aws:s3:::test-727250514989/*"]
+    }
+    statement {
+        effect = "Allow"
+        actions = ["rekognition:DetectFaces"]
+        resources = ["*"]
+    }
+
+    statement {
+        sid = "SpecificTable"
+        effect = "Allow"
+        actions = ["dynamodb:BatchWrite*"]
+        resources =["arn:aws:dynamodb:*:*:table/images-meta"]
+
+    }
+}
+
+resource "aws_iam_role" "gr4_lambda_role" {
+  name   = "gr4-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.gr4_lambda_assume_role_policies.json
+}
+
+resource "aws_iam_policy" "gr4_lambda_policy" {
+  name = "gr4-lambda-policy"
+  policy = data.aws_iam_policy_document.gr4_lambda_policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "gr4_lambda_role_policy_attach" {
+    role = aws_iam_role.gr4_lambda_role.name
+    policy_arn = aws_iam_policy.gr4_lambda_policy.arn
+}
